9d932135763cd500c2b98d485be9acbea482f16f
aboiko1281 <aboiko1281@gmail.com>
2025-10-28 17:15:29 +0000

    fix: server

--- backend_node/middleware/auth.js
+++ backend_node/middleware/auth.js
@@ -10,9 +10,13 @@ module.exports = (req, res, next) => {
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Origin, X-Requested-With, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
 
-  // Перевірка авторизації для інших методів
-  // if (req.headers.authorization !== `Bearer ${process.env.SECRET_KEY}`) {
-  //   return res.status(401).json({ error: "Unauthorized" });
-  // }
+   // Авторизація
+  //  const authHeader = req.headers.authorization;
+  //  const expectedToken = `Bearer ${process.env.SECRET_KEY}`;
+ 
+  //  if (!authHeader || authHeader !== expectedToken) {
+  //    return res.status(401).json({ error: "Unauthorized - Authentication Required" });
+  //  }
+ 
   next();
 };
